8db8f3dc891cde1b46c771be1f59b768bbf70b50
erikvella <vella.erik1@gmail.com>
2023-10-10 20:35:51 +0100

    [fix] main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,6 +8,8 @@ name: 'Main'
   <main>
 
     <div class="container-main d-flex ">
+
+
       <div class="card">
         
         <span class="discount">-50&percnt;</span>
@@ -18,7 +20,67 @@ name: 'Main'
               <span class="red-price">&euro;14.98</span>
               <span class="barred-price">&euro;29.99</span>
             </div>
-            
+
+      </div>
+      <div class="card">
+        
+        <span class="discount">-50&percnt;</span>
+            <span class="heart">&hearts;</span>
+            <div class="descr">
+              <span class="marca">Levi's</span>
+              <h3>RELAXED FIT TEE UNISEX</h3>
+              <span class="red-price">&euro;14.98</span>
+              <span class="barred-price">&euro;29.99</span>
+            </div>
+
+      </div>
+      <div class="card">
+        
+        <span class="discount">-50&percnt;</span>
+            <span class="heart">&hearts;</span>
+            <div class="descr">
+              <span class="marca">Levi's</span>
+              <h3>RELAXED FIT TEE UNISEX</h3>
+              <span class="red-price">&euro;14.98</span>
+              <span class="barred-price">&euro;29.99</span>
+            </div>
+
+      </div>
+      <div class="card">
+        
+        <span class="discount">-50&percnt;</span>
+            <span class="heart">&hearts;</span>
+            <div class="descr">
+              <span class="marca">Levi's</span>
+              <h3>RELAXED FIT TEE UNISEX</h3>
+              <span class="red-price">&euro;14.98</span>
+              <span class="barred-price">&euro;29.99</span>
+            </div>
+
+      </div>
+      <div class="card">
+        
+        <span class="discount">-50&percnt;</span>
+            <span class="heart">&hearts;</span>
+            <div class="descr">
+              <span class="marca">Levi's</span>
+              <h3>RELAXED FIT TEE UNISEX</h3>
+              <span class="red-price">&euro;14.98</span>
+              <span class="barred-price">&euro;29.99</span>
+            </div>
+
+      </div>
+      <div class="card">
+        
+        <span class="discount">-50&percnt;</span>
+            <span class="heart">&hearts;</span>
+            <div class="descr">
+              <span class="marca">Levi's</span>
+              <h3>RELAXED FIT TEE UNISEX</h3>
+              <span class="red-price">&euro;14.98</span>
+              <span class="barred-price">&euro;29.99</span>
+            </div>
+
       </div>
 
       
